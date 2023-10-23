9e7536df3b5c970af0ea3cd5f52d40e059074001
https://github.com/ghost160111 <jalol100.ghost@yandex.com>
2023-10-23 23:10:55 +0500

    integrated webpack plugin to build project

---
+++ lib/ts/test.ts
@@ -0,0 +1,9 @@
+"use strict";
+
+abstract class Tests {
+  public static test1(): void {}
+  public static test2(): void {}
+  public static test3(): void {}
+}
+
+export default Tests;
---
+++ webpack.config.js
@@ -0,0 +1,129 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+
+const fontsDir = path.resolve(__dirname, "/src/assets/fonts");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const disableExtensions = (...extensions) => {
+  if (extensions) {
+    return [...extensions]
+  }
+}
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: "all"
+    }
+  }
+
+  if (isProd) {
+    config.minimizer = [
+      new CssMinimizerWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ]
+  }
+
+  return config;
+}
+
+const fileName = ext => isDev ? `[name].${ext}` : `[name].[fullhash].${ext}`;
+
+const cssLoader = (extra) => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+    },
+    "css-loader"
+  ]
+
+  if (extra) {
+    loaders.push(extra);
+  }
+  
+  return loaders;
+}
+
+const babelOptions = (option) => {
+  const options = {
+    presets: ["@babel/preset-env"]
+  }
+
+  if (option) {
+    options["presets"].push("@babel/preset-typescript");
+  }
+
+  return options;
+}
+
+module.exports = {
+  context: path.resolve(__dirname, "lib"),
+  mode: "development",
+  entry: {
+    main: "./index.js",
+  },
+  output: {
+    filename: fileName("js"),
+    path: path.resolve(__dirname, "dist")
+  },
+  resolve: {
+    extensions: disableExtensions(".js", ".ts"),
+    alias: {
+      "@": path.resolve(__dirname, "lib")
+    }
+  },
+  optimization: optimization(),
+  devtool: isDev ? "source-map" : false,
+  devServer: {
+    port: 5128
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: "./index.html",
+    }),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     { from: "assets/images", to: "assets/images" }
+    //   ]
+    // }),
+    new MiniCssExtractPlugin({
+      filename: fileName("css")
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: cssLoader()
+      },
+      {
+        test: /\.(sass|scss)$/i,
+        use: cssLoader("sass-loader")
+      },
+      {
+        test: /\.(js|mjs)$/,
+        exclude: "/node_modules",
+        use: {
+          loader: "babel-loader",
+          options: babelOptions()
+        }
+      },
+      {
+        test: /\.ts$/,
+        exclude: "/node_modules",
+        use: {
+          loader: "babel-loader",
+          options: babelOptions("ts")
+        }
+      }
+    ],
+  }
+}
