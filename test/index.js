491aa26426dfce0bce4b04a56e749def5f76235a
Developer <130534010+ghost160111@users.noreply.github.com>
2023-10-26 19:28:22 +0500

    junk commit, need to be deleted

--- test/index.js
+++ test/index.js
@@ -1,99 +1,61 @@
-class ArrayTools {
-}
-class ObjectTools {
-}
-class ConsoleTools {
-}
-class Scripts {
-}
-class Ajax {
-}
-class Attributes {
-}
-class Events {
-}
-class Manipulation {
-}
-class Selecting {
-}
-class Styles {
-}
-class Traversing {
-}
-class ExceptionHandler {
-}
-class Types {
-}
-class DOMTools {
+// Regular Expressions
+
+/**
+ * @param {string} flag
+ * @param {string} description
+ * @param {string} correspondingProperty
+ */
+const regex = [];
+
+const addRegexDescription = (flag, description, correspondingProperty) => {
+  regex.push({
+    flag,
+    description,
+    correspondingProperty
+  });
 }
 
-class ClassCollection {
-  static list = [
-    ArrayTools,
-    ObjectTools,
-    ConsoleTools,
-    Scripts,
-    Ajax,
-    Attributes,
-    Events,
-    Manipulation,
-    Selecting,
-    Styles,
-    Traversing,
-    ExceptionHandler,
-    Types,
-    DOMTools
-  ]
-
-  /** @param {object[]} className */
-  static setClassConstructor(className) {
-    for (let i = 0; i < className.length; ++i) {
-      this.list.push(className[i]);
-    }
+const regexFlags = {
+  d: {
+    flag: "d",
+    description: "Generate indices for substring matches",
+    correspondingProperty: "hasIndices"
+  },
+  g: {
+    flag: "g",
+    description: "Global search",
+    correspondingProperty: "global"
+  },
+  i: {
+    flag: "i",
+    description: "Case-insentive search.",
+    correspondingProperty: "ignoreCase"
+  },
+  m: {
+    flag: "m",
+    description: "Allows ^ and $ to match newline characters",
+    correspondingProperty: "multiline"
+  },
+  s: {
+    flag: "s",
+    description: "Allows . to match newline characters",
+    correspondingProperty: "dotAll"
+  },
+  u: {
+    flag: "u",
+    description: "\"Unicode\";treat a pattern as a sequence of Unicode code points",
+    correspondingProperty: "unicode"
+  },
+  v: {
+    flag: "v",
+    description: "An upgrade to the u mode with more Unicode features",
+    correspondingProperty: "unicodeSets"
+  },
+  y: {
+    flag: "y",
+    description: "Perform a \"sticky\" search that matches starting at the current position in the target string",
+    correspondingProperty: "sticky"
   }
+};
 
-  /** @param {string} className */
-  static getClassConstructor(className) {
-    for (let i = 0; this.list.length; ++i) {
-      try {
-        if (className === this.list[i].name) {
-          return this.list[i].name;
-        }
-      } catch (error) {
-        throw Error(`${error.stack}`);
-      }
-    }
-  }
-}
-
-class Constructor {
-
-}
-
-class Class {
-
-}
-
-ClassCollection.setClassConstructor([Constructor, Class])
-
-console.log(ClassCollection.getClassConstructor("Constructor"));
-
-function sumTo(n) {
-  let sum = 0;
-
-  for (let i = 0; i <= n; ++i) {
-    sum += i;
-  }
-
-  return sum;
-}
-
-const results = [
-  sumTo(1),
-  sumTo(2),
-  sumTo(3),
-  sumTo(100000)
-];
-
-console.log(results); // [ 1, 3, 6, 5000050000 ]
-
+console.log(regexFlags);
